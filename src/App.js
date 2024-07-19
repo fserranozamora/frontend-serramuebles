80a40d747b495fbafba79c0379c25c3f8e5a62cf
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2024-07-18 20:24:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from "react";
-import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import { BrowserRouter as Router,Route,Routes,Navigate } from "react-router-dom";
 import Login from "./paginas/auth/Login";
 import Registro from "./paginas/auth/Registro";
 import Home from "./Home";
@@ -12,6 +12,7 @@ import EditarInsumos from "./paginas/modulos/insumos/EditarInsumos";
 import MostrarMateriales from "./paginas/modulos/materiales/MostrarMateriales";
 import AgregarMateriales from "./paginas/modulos/materiales/AgregarMateriales";
 import EditarMateriales from "./paginas/modulos/materiales/EditarMateriales";
+import RutasProtegidas from "./paginas/auth/RutasProtegidas";
 
 function App() {
   return (
@@ -19,18 +20,19 @@ function App() {
       <Fragment>
         <Router>
           <Routes>
-            <Route path="/" exact element ={<Login/>}></Route>
+            <Route path="/" element ={<Navigate to = "/login"/>}></Route>
+            <Route path="/login" exact element ={<Login/>}></Route>
             <Route path="/Registro" exact element ={<Registro/>}></Route>
-            <Route path="/home" exact element ={<Home/>}></Route>
-            <Route path="/herramientas" exact element ={<MostrarHerramientas/>}></Route>
-            <Route path="/herramientas/agregar" exact element ={<AgregarHerramientas/>}></Route>
-            <Route path="/herramientas/editar/:id" exact element ={<EditarHerramientas/>}></Route>
-            <Route path="/insumos" exact element ={<MostrarInsumos/>}></Route>
-            <Route path="/insumos/agregar" exact element ={<AgregarInsumos/>}></Route>
-            <Route path="/insumos/editar/:id" exact element ={<EditarInsumos/>}></Route>
-            <Route path="/materiales" exact element ={<MostrarMateriales/>}></Route>
-            <Route path="/materiales/agregar" exact element ={<AgregarMateriales/>}></Route>
-            <Route path="/materiales/editar/:id" exact element ={<EditarMateriales/>}></Route>
+            <Route path="/home" exact element ={<RutasProtegidas element={<Home/>}/>}></Route>
+            <Route path="/herramientas" exact element ={<RutasProtegidas element={<MostrarHerramientas/>}/>}></Route>
+            <Route path="/herramientas/agregar" exact element ={<RutasProtegidas element={<AgregarHerramientas/>}/>}></Route>
+            <Route path="/herramientas/editar/:id" exact element ={<RutasProtegidas element={<EditarHerramientas/>}/>}></Route>
+            <Route path="/insumos" exact element ={<RutasProtegidas element={<MostrarInsumos/>}/>}></Route>
+            <Route path="/insumos/agregar" exact element ={<RutasProtegidas element={<AgregarInsumos/>}/>}></Route>
+            <Route path="/insumos/editar/:id" exact element ={<RutasProtegidas element={<EditarInsumos/>}/>}></Route>
+            <Route path="/materiales" exact element ={<RutasProtegidas element={<MostrarMateriales/>}/>}></Route>
+            <Route path="/materiales/agregar" exact element ={<RutasProtegidas element={<AgregarMateriales/>}/>}></Route>
+            <Route path="/materiales/editar/:id" exact element ={<RutasProtegidas element={<EditarMateriales/>}/>}></Route>
           </Routes>
         </Router>
       </Fragment>
