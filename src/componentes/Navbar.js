db35f033a8e9c65dfbec6ce67e8977f43d08b02f
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2026-07-22 17:28:03 -0500

    Remove logout option from Navbar
    Removed the logout functionality from the Navbar component.

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -2,22 +2,15 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-
+// eslint-disable-next-line
     const navigate = useNavigate();
 
-    const cerrarSesion = () => {
-        localStorage.removeItem("token");
-        navigate("/");
-    }
 
     return (
         <nav className="main-header navbar navbar-expand navbar-white navbar-light">
             <ul className="navbar-nav">
                 <li className="nav-item">
                     <Link to={"#"} className="nav-link" data-widget="pushmenu" role="button"><i className="fas fa-bars" /></Link>
-                </li>
-                <li className="nav-item d-none d-sm-inline-block">
-                    <strong onClick={cerrarSesion} className="nav-link" style={{cursor: 'pointer'}}>Cerrar sesión</strong>
                 </li>
             </ul>
             <ul className="navbar-nav ml-auto">
