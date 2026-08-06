da16fee4972c9eeb85acf62bc4c49675bdf5d5df
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2026-08-06 17:58:32 -0500

    Remove unused useNavigate import from Navbar.js

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-// eslint-disable-next-line
-    const navigate = useNavigate();
-
-
     return (
         <nav className="main-header navbar navbar-expand navbar-white navbar-light">
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <Link to={"#"} className="nav-link" data-widget="pushmenu" role="button"><i className="fas fa-bars" /></Link>
+                    <Link to={"#"} className="nav-link" data-widget="pushmenu" role="button">
+                        <i className="fas fa-bars" />
+                    </Link>
                 </li>
             </ul>
             <ul className="navbar-nav ml-auto">
