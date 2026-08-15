151bfcdcd16ddb1298cd4c80ab62fea6b4eefc6b
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2026-08-15 18:15:13 -0500

    Update footer version from 1.0.7 to 1.0.8

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="main-footer">
             <div className="float-right d-none d-sm-block">
-                <b>Version</b> 1.0.7
+                <b>Version</b> 1.0.8
             </div>
             <strong> Copyright © 2026 - Laborarorios Serrano S.A.S. </strong>
         </footer>
