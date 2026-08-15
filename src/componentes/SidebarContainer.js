d583489aeaf301b7d0c9c203bcd945974741b84e
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2026-08-15 18:46:26 -0500

    Improve SidebarContainer layout and formatting
    Updated alignment and formatting for better readability.

--- src/componentes/SidebarContainer.js
+++ src/componentes/SidebarContainer.js
@@ -8,9 +8,9 @@ const SidebarContainer = () => {
     return (
         <aside className="main-sidebar sidebar-dark-primary elevation-4">
             <Link to={"/home"} className="brand-link">
-                <img src={logo} className="App-logo1" alt="logo" width={'200px'} />
+                <img src={logo} className="App-logo1" alt="logo" width={'200px'}/>
                 <span><p></p></span>
-                <span className="brand-text font-weight-light"><p>Control de inventarios</p></span>
+                <span className="brand-text font-weight-light"><p align={ 'center' }>Control de inventarios</p></span>
             </Link>
             <div className="sidebar">
                 <div className="user-panel mt-3 pb-3 mb-3 d-flex">
@@ -21,7 +21,7 @@ const SidebarContainer = () => {
                        &nbsp; 
                     </div>
                     <div className="info">
-                        <Link to={"/home"} className="d-block"><i className="nav-icon fas fa-th" /><b> Menu Principal </b></Link>
+                        <Link to={"/home"} className="d-block"><i className="nav-icon fas fa-th text-truncate" /><b> Menu Principal </b></Link>
                     </div>
                 </div>
                 <Menu></Menu>
